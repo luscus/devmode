de92f7e1ccee65fa9ca51d7f7d5dfa8f0a5ad302
luscus <luscus.redbeard@gmail.com>
2016-03-10 13:29:51 +0100

    added terminal support

--- bin/devmode.js
+++ bin/devmode.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 'use strict';
+console.log('process.env.PWD:', process.env.PWD);
+console.log('process.argv:', process.argv);
+console.log('process.cwd:', process.cwd());
+console.log('__dirname:', __dirname);
 
 var program = require('commander');
+var cli     = require('../lib/cli');
 var pack    = require('../package.json');
+var devmode = require('../' + pack.main);
 
 program
   .version(pack.version)
-  .usage('<path/to/appFile1> [, <path/to/app2>]');
+  .description('Starts one or more Node.js applications')
+  .usage('<path> [otherPaths...]')
+  .action(function () {
+    var options = cli.parse(process.argv);
+    devmode(options);
+  })
+  .parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
